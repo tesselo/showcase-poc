de794a48ed109630dd274da3e83db0b8a16b5512
Vitor Paixão <vitornvpaixao@gmail.com>
2022-02-23 16:19:20 +0000

    feat(showcase): add vue showcase poc project

---
+++ vue3-leaflet-vuetify/src/components/HelloTessera.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      {{ subMsg }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloTessera',
+  props: {
+    msg: String,
+    subMsg: String,
+  },
+});
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
