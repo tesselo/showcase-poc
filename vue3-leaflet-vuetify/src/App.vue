76cfa89db389ddb8200ba77b9c98e337a8b3abb7
Vitor Paixão <vitornvpaixao@gmail.com>
2022-02-23 16:19:20 +0000

    feat(showcase): add vue showcase poc project

---
+++ vue3-leaflet-vuetify/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloTessera :msg="helloMsg" :subMsg="subHelloMsg"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloTessera from './components/HelloTessera.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloTessera
+  },
+  data() {
+    return {
+      helloMsg: 'Hello Tesseras!',
+      subHelloMsg: 'This is the first Tesselo Showcase POC.'
+    }
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
