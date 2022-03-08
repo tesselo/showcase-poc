a7402f995ac87633ea9dee49b2161cd5779d1228
Vitor Paixão <vitornvpaixao@gmail.com>
2022-03-08 09:57:50 +0000

    refactor(tsl-toolbar): removed unnecessary css

--- vue3-leaflet-vuetify/src/components/Tsl-Toolbar.vue
+++ vue3-leaflet-vuetify/src/components/Tsl-Toolbar.vue
@@ -42,40 +42,4 @@ export default defineComponent({
 .v-toolbar {
   z-index: 1;
 }
-
-.v-application__wrap > .v-toolbar.v-toolbar--density-default > .v-toolbar__content {
-  padding-top: 0px;
-  padding-bottom: 0px;
-
-  .v-img__img {
-    left: 0px
-  }
-}
-
-.toolbar-switch-btn {
-  margin-top: 39px;
-
-  .v-input__control > .v-selection-control {
-    
-    .v-selection-control__wrapper > .v-switch__track {
-      height: 9px;
-      width: 18px;
-    }
-
-    .v-selection-control__input {
-      width: 27px;
-      height: 27px;
-      
-      .v-switch__thumb {
-        height: 14px;
-        width: 14px;
-      }
-    } 
-
-    .v-label {
-      font-size: 14px
-    }
-
-  }
-}
 </style>
